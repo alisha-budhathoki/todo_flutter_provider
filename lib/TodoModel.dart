90fb1f6e529bdd71bb4c34e753db4d4534362223
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-02-11 11:56:42 +0545

    adding dialog along with provider

--- lib/TodoModel.dart
+++ lib/TodoModel.dart
@@ -4,12 +4,21 @@ import 'package:provider_todo/TaskModel.dart';
 class TodoModel extends ChangeNotifier{
   List<TaskModel> taskList = []; //contians all the task
 
-  addTaskInList(){
-    TaskModel taskModel = TaskModel("title ${taskList.length}", "this is the task no detail ${taskList.length}");
-    taskList.add(taskModel);
+  addTaskInList(String sourceInput){
+    //notifying
+    // print('snjbjdbdshj'+ sourceInput.toString());
+    // TaskModel taskModel = TaskModel(sourceInput.toString()+"title ${taskList.length}", "this is the task no detail ${taskList.length}");
+    // taskList.add(taskModel);
+    if(sourceInput.toString() == null){
+      TaskModel taskModel = TaskModel("title ${taskList.length} ", "this is the task no detail ${taskList.length}");
+      taskList.add(taskModel);
+    }
+    else{
+      TaskModel taskModel = TaskModel(sourceInput.toString()+" title ${taskList.length}", "this is the task no detail ${taskList.length}");
+      taskList.add(taskModel);
+    }
 
     notifyListeners();
-    //code to do
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,38 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: ChangeNotifierProvider(
-        builder: (context) => TodoModel(),
-        child: MyHomePage(),
-      )
-
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider(
+          builder: (context) => TodoModel(),
+          child: MyHomePage(),
+        ));
   }
 }
 
-class MyHomePage extends StatelessWidget {
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String sourceInput;
+TodoModel provider;
+  @override
+  void initState() {
+    super.initState();
+     provider = Provider.of<TodoModel>(context,listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,66 +45,143 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text("Todo Application", style: TextStyle(color: Colors.white),),
-        leading: IconButton(icon: Icon(Icons.menu, color: Colors.white70,),),
+        title: Text(
+          "Todo Application",
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white70,
+          ),
+        ),
       ),
       body: Column(
         children: <Widget>[
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: 20,),
-
-              Text("02 : 36 PM", style: TextStyle(color: Colors.white70, fontSize: 45, fontWeight: FontWeight.bold),),
-
-              Text("current time", style: TextStyle(color: Colors.white, fontSize: 16,),),
-
-              SizedBox(height: 20,)
-
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "02 : 36 PM",
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "current time",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              )
             ],
           ), //to show the clock
 
           Expanded(
             child: Container(
-
-              decoration: BoxDecoration(borderRadius: BorderRadius.only(topRight: Radius.circular(50), topLeft: Radius.circular(60)), color: Colors.white),
-              child: Consumer<TodoModel>(
-                builder: (context, todo, child){
-                  return ListView.builder(
-                      itemCount: todo.taskList.length,
-                      itemBuilder: (context, index){
-                        return Container(
-                          child: ListTile(
-                            contentPadding: EdgeInsets.only(left: 32, right: 32, top: 8, bottom: 8),
-                            title: Text(todo.taskList[index].title, style : TextStyle(color: Colors.black87,
-                                fontWeight: FontWeight.bold),),
-                            subtitle: Text(todo.taskList[index].detail, style: TextStyle(color: Colors.black45,
-                                fontWeight: FontWeight.bold),),
-
-                            trailing: Icon(Icons.check_circle, color: Colors.greenAccent,),
-                          ),
-                          margin: EdgeInsets.only(bottom: 8, left: 16, right: 16),
-                        );
-                      }
-                  );
-                },
-              )
-
-
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        topLeft: Radius.circular(60)),
+                    color: Colors.white),
+                child: Consumer<TodoModel>(
+                  builder: (context, todo, child) {
+                    return ListView.builder(
+                        itemCount: todo.taskList.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            child: ListTile(
+                              contentPadding: EdgeInsets.only(
+                                  left: 32, right: 32, top: 8, bottom: 8),
+                              title: Text(
+                                todo.taskList[index].title,
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                todo.taskList[index].detail,
+                                style: TextStyle(
+                                    color: Colors.black45,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              trailing: Icon(
+                                Icons.check_circle,
+                                color: Colors.greenAccent,
+                              ),
+                            ),
+                            margin:
+                                EdgeInsets.only(bottom: 8, left: 16, right: 16),
+                          );
+                        });
+                  },
+                )),
+          ),
+          Container(
+            color: Colors.white,
+            child: TextField(
+              decoration: InputDecoration(hintText: 'Enter a an item to post'),
+              onChanged: (val){
+                sourceInput= val;
+              },
             ),
-          )
-
+          ),
         ],
       ),
-
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton:FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){
-            Provider.of<TodoModel>(context).addTaskInList();
+        onPressed: () {
+          _showDialog(context: context);
+          // Provider.of<TodoModel>(context).addTaskInList(sourceInput);
         },
       ),
+    );
+  }
 
+  void _showDialog({BuildContext context}) {
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return  ChangeNotifierProvider.value(
+          value: provider,
+          child:  AlertDialog(
+            title: new Text("Enter a text"),
+            content: new TextField(
+              decoration: InputDecoration(hintText: 'Enter a search term'),
+              onChanged: (val) {
+                sourceInput = val;
+              },
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: new Text("Okay"),
+                onPressed: () {
+                  print('Okay pressed');
+                  provider.addTaskInList(sourceInput);
+                  Navigator.of(context);
+                  // Provider.of<TodoModel>(context).addTaskInList(sourceInput);
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
+
 
